867d2417917b3d0c623f7a9c2c923e7d2edec23c
Teacher <teacher@tim.local>
2016-03-06 21:00:43 -0500

    Added a view controller

---
+++ thortonfun/TimsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TimsViewController.swift
+//  thortonfun
+//
+//  Created by MLTI Kit 3 on 3/6/16.
+//  Copyright © 2016 9red7. All rights reserved.
+//
+
+import UIKit
+
+class TimsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
